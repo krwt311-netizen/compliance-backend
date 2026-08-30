a9913bc1ac91ec711cdf74f1361fb7d72f437355
krwt311 <krw311@gmail.com>
2026-08-30 20:48:57 +0530

    Initial backend setup with FastAPI CRUD and Scan model

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import HTTPException
+from models import Scan , FieldCheck
+from  fastapi import FastAPI
+app = FastAPI()
+
+    
+scans = [
+    Scan(
+        id=1,
+        mrp=FieldCheck(value="₹99, inclusive of all taxes", is_valid=True, message="OK"),
+        net_quantity=FieldCheck(value="500g", is_valid=True, message="OK"),
+        manufacturer_address=FieldCheck(value="XYZ Pvt Ltd, Delhi", is_valid=True, message="OK"),
+        mfg_date=FieldCheck(value="not found", is_valid=False, message="Missing on label"),
+        consumer_care=FieldCheck(value="care@xyz.com", is_valid=True, message="OK"),
+        unit_of_measurement=FieldCheck(value="g", is_valid=True, message="OK"),
+        overall_status="Non-Compliant"
+    )
+]
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Scan API!"}
+@app.get("/scans")
+def get_all_scans():
+    return scans
+
+
+
+@app.get("/scans/{id}")
+def get_scan_by_id(id: int):
+    for i in range(len(scans)):
+        if scans[i].id == id:
+            return scans[i]
+    raise HTTPException(status_code=404, detail="scan not found")   
+
+@app.post("/scans")
+def add_scan(scan: Scan):
+    scans.append(scan)
+    return {"message": "Scan added successfully."}   
+
+@app.put("/scans/{id}")
+def update_scan(id: int, scan: Scan):
+    for i in range(len(scans)):
+        if scans[i].id == id:
+            scans[i] = scan
+            return {"message": "Scan updated successfully."}
+    raise HTTPException(status_code=404, detail="invalid scan id")    
+
+@app.delete("/scans/{id}")
+def delete_scan(id: int):
+    for i in range(len(scans)):
+        if scans[i].id == id:
+            del scans[i]
+            return {"message": "Scan deleted successfully."}
+    raise HTTPException(status_code=404, detail="unavailable")
+
---
+++ models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Optional
+class FieldCheck(BaseModel):
+    value: str
+    is_valid: bool
+    message: str   
+class Scan(BaseModel):
+   id:int 
+   mrp:FieldCheck
+   net_quantity:FieldCheck
+   manufacturer_address: FieldCheck
+   mfg_date: FieldCheck
+   consumer_care: FieldCheck
+   unit_of_measurement: FieldCheck
+   country_of_origin: Optional[FieldCheck] = None
+   overall_status: str
